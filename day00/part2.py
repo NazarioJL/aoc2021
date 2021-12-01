060fe157727f2c406153f8e35275dc422b22a7ce
Johnny Nazario <jnaz@amazon.com>
2021-11-30 23:39:16 -0800

    Add day1 problem

--- day00/part2.py
+++ day00/part2.py
@@ -5,16 +5,38 @@ import os.path
 
 import pytest
 from more_itertools import pairwise
+from more_itertools import sliding_window
 
 from support import timing
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
 
 
+def compute_2(s: str) -> int:
+    numbers = [int(line) for line in s.splitlines()]
+    result = 0
+    curr = 0
+    prev = 0
+
+    for i, e in enumerate(numbers):
+        if i <= 3 - 1:
+            curr += e
+        else:
+            new_curr = curr + e - numbers[prev]
+            if new_curr > curr:
+                result += 1
+            curr = new_curr
+            prev += 1
+
+    return result
+
+
 def compute(s: str) -> int:
     numbers = [int(line) for line in s.splitlines()]
-    result = sum(0 if a > b else 1 for a, b in pairwise(numbers))
-    return result
+    return sum(
+        1 if b > a else 0
+        for a, b in pairwise(sum(w) for w in sliding_window(numbers, 3))
+    )
 
 
 INPUT_S = """\
@@ -33,7 +55,7 @@ INPUT_S = """\
 
 @pytest.mark.parametrize(
     ("input_s", "expected"),
-    ((INPUT_S, 7),),
+    ((INPUT_S, 5),),
 )
 def test(input_s: str, expected: int) -> None:
     assert compute(input_s) == expected
--- support/support.py
+++ support/support.py
@@ -9,58 +9,58 @@ from typing import Generator
 
 
 @contextlib.contextmanager
-def timing(name: str = '') -> Generator[None, None, None]:
+def timing(name: str = "") -> Generator[None, None, None]:
     before = time.time()
     try:
         yield
     finally:
         after = time.time()
         t = (after - before) * 1000
-        unit = 'ms'
+        unit = "ms"
         if t < 100:
             t *= 1000
-            unit = 'μs'
+            unit = "μs"
         if name:
-            name = f' ({name})'
-        print(f'> {int(t)} {unit}{name}')
+            name = f" ({name})"
+        print(f"> {int(t)} {unit}{name}")
 
 
 def get_input(year: int, day: int) -> str:
-    with open('../.env') as f:
+    with open("../.env") as f:
         contents = f.read()
 
-    url = f'https://adventofcode.com/{year}/day/{day}/input'
-    req = urllib.request.Request(url, headers={'Cookie': contents.strip()})
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
+    req = urllib.request.Request(url, headers={"Cookie": contents.strip()})
     return urllib.request.urlopen(req).read().decode()
 
 
 def download_input() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('year', type=int)
-    parser.add_argument('day', type=int)
+    parser.add_argument("year", type=int)
+    parser.add_argument("day", type=int)
     args = parser.parse_args()
 
     for i in range(5):
         try:
             s = get_input(args.year, args.day)
         except urllib.error.URLError as e:
-            print(f'zzz: not ready yet: {e}')
+            print(f"zzz: not ready yet: {e}")
             time.sleep(1)
         else:
             break
     else:
-        raise SystemExit('timed out after attempting many times')
+        raise SystemExit("timed out after attempting many times")
 
-    with open('input.txt', 'w') as f:
+    with open("input.txt", "w") as f:
         f.write(s)
 
     lines = s.splitlines()
     if len(lines) > 10:
         for line in lines[:10]:
             print(line)
-        print('...')
+        print("...")
     else:
         print(lines[0][:80])
-        print('...')
+        print("...")
 
     return 0
