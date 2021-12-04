fe947b2c8bbaa219b041f36dfa2b4ba3ca310070
Johnny Nazario <jnaz@amazon.com>
2021-12-04 02:54:51 -0800

    Add day4 problem

---
+++ day04/board.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class BingoBoard:
+    board_id: int
+    numbers: dict[int, tuple[int, int]]
+    rows: list[int]
+    cols: list[int]
+
+    def mark_number(self, n: int) -> bool:
+        r, c = self.numbers[n]
+        self.rows[r] -= 1
+        self.cols[c] -= 1
+
+        del self.numbers[n]
+
+        return self.rows[r] == 0 or self.cols[c] == 0
+
+    def pretty_print(self) -> None:
+        board = [[" *" for _ in range(5)] for _ in range(5)]
+        for n, (r, c) in self.numbers.items():
+            board[r][c] = f"{n:2}"
+
+        print(f"==Board({self.board_id:04})==")
+        for row in board:
+            print(" ".join(row))
+        print("===============")
---
+++ day04/part1.py
@@ -0,0 +1,102 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+from support import timing
+
+from day04.board import BingoBoard
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    line_count = len(lines)
+    drawn_nums = [int(n) for n in lines[0].split(",")]
+
+    board_count = (line_count - 1) // 6
+    boards_lookup: dict[int, list[BingoBoard]] = {}
+    boards = []
+
+    for board_idx in range(board_count):
+        start = (6 * board_idx) + 1 + 1  # first line and empty space
+        end = start + 5
+
+        board = BingoBoard(board_id=board_idx, numbers={}, rows=[5, 5, 5, 5, 5], cols=[5, 5, 5, 5, 5])
+        boards.append(board)
+        board_lines = lines[start:end]
+
+        all_nums = []
+
+        for row, bl in enumerate(board_lines):
+            for col, n in enumerate(int(n_s) for n_s in bl.split()):
+                all_nums.append(n)
+                if n not in boards_lookup:
+                    boards_lookup[n] = []
+                boards_lookup[n].append(board)
+                board.numbers[n] = row, col
+
+    winning_board: BingoBoard | None = None
+    winning_num: int | None = None
+
+    for num in drawn_nums:
+        if winning_board is not None:
+            break
+        if num in boards_lookup:
+            for board in boards_lookup[num]:
+                if board.mark_number(num):
+                    winning_board = board
+                    winning_num = num
+
+    if winning_board is None:
+        raise ValueError("No winning board found")
+
+    return sum(winning_board.numbers.keys() or [0]) * (winning_num or 0)
+
+
+INPUT_S = """\
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 4512),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
